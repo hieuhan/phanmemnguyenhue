a16e8c1350e4352d31cacc33f62351aaa4f4dfd8
hieuhan <hantrunghieu@gmail.com>
2023-02-08 11:22:39 +0700

    update xe.choto.com

---
+++ web-scraping/xe.chotot.com/index.js
@@ -0,0 +1,32 @@
+const browserObject = require('../browser');
+const scraperObject = require('./scraper');
+const fs = require('fs');
+
+(async () => {
+    
+    let browser = await browserObject.startBrowser();
+
+    if(browser != null)
+    {
+        let categories = [];
+        let rawdata = fs.readFileSync('./categories.txt');
+        categories = JSON.parse(rawdata);
+
+        await Promise.all([
+            scraperObject.scraper(browser, categories, 'https://xe.chotot.com/mua-ban-xe'),
+            scraperObject.scraper(browser, categories, 'https://xe.chotot.com/mua-ban-xe-may'),
+            scraperObject.scraper(browser, categories, 'https://xe.chotot.com/mua-ban-xe-tai-xe-ben'),
+            scraperObject.scraper(browser, categories, 'https://xe.chotot.com/mua-ban-xe-dien'),
+            scraperObject.scraper(browser, categories, 'https://xe.chotot.com/mua-ban-xe-dap'),
+            scraperObject.scraper(browser, categories, 'https://xe.chotot.com/mua-ban-phuong-tien-khac'),
+            scraperObject.scraper(browser, categories, 'https://xe.chotot.com/mua-ban-phu-tung-xe')
+        ]);
+
+        console.log('Đóng trình duyệt...');
+
+        await browser.close();
+    }
+
+})().catch(error => {
+    console.error(`Không thể tạo phiên bản trình duyệt => ${error}\n`);
+});
