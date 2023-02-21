018b9d1e570123ef39372aef1b14efbb87a60370
hieuhan <hantrunghieu@gmail.com>
2023-02-21 10:27:41 +0700

    ...

--- web-application/phanmemnguyenhue.library/Customers.cs
+++ web-application/phanmemnguyenhue.library/Customers.cs
@@ -16,6 +16,7 @@ namespace phanmemnguyenhue.library
         public int SiteId { get; set; }
         public string FullName { get; set; }
         public string PhoneNumber { get; set; }
+        public string SecondPhoneNumber { get; set; }
         public string Email { get; set; }
         public string Avatar { get; set; }
         public string Profile { get; set; }
